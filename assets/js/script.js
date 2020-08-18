702451988f123ccf15a1d7849336009592bc0d6e
Derrick Hepworth <derrickhepworth@gmail.com>
2020-08-18 16:43:48 -0600

    event listener with form values to generate task

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,33 @@
 var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
-var createTaskHandler = function() {
+
+// Function called on event submit from event Listener below
+var createTaskHandler = function () {
     event.preventDefault();
+    
+    var taskNameInput = document.querySelector("input[name='task-name']").value;
+    var taskTypeInput = document.querySelector("select[name='task-type']").value;
+
+    // creates <li> item
     var listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
-    listItemEl.textContent = "This is a new task.";
+
+    //create div to hold task info and add to list item
+    var taskInfoEl = document.createElement("div");
+
+    //class name 
+    taskInfoEl.className = 'task-info';
+
+    // add HTML content to div
+    taskInfoEl.innerHTML = "<h3 class = 'task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>";
+
+    //add div(with html generated content and value from input) to task below task (CSS)
+    listItemEl.appendChild(taskInfoEl);
+
+    // add task to list (has value from input and div with HTM elemnts(to be formatted by CSS) with type from select inside)
     tasksToDoEl.appendChild(listItemEl);
-    console.log('"add task" event:', event);
+
 }
 
 formEl.addEventListener("submit", createTaskHandler);
