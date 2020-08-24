1021d4275ee103c4ecca4c6e310a7f21507e11b4
Derrick Hepworth <derrickhepworth@gmail.com>
2020-08-24 11:37:09 -0600

    drag html attribute added dynamically

--- assets/js/script.js
+++ assets/js/script.js
@@ -114,6 +114,7 @@ var createTaskEl = function (taskDataObj) {
 
     // add task id as a custom attribute
     listItemEl.setAttribute("data-task-id", taskIdCounter);
+    listItemEl.setAttribute("draggable", "true")
 
     //create and append div and h3 for task. Task box in column with values from form (vars)
     var taskInfoEl = document.createElement("div");
